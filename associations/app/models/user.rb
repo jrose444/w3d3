3ae66cfb94fd1fde46387758ee808343a361b3e0
App Academy Student <student@appacademy.io>
2019-05-29 14:56:06 -0700

    git original init

---
+++ associations/app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  has_many :enrollments,
+    foreign_key: :student_id,
+    primary_key: :id,
+    class_name: 'Enrollment'
+
+  has_many :enrolled_courses,
+    through: :enrollments,
+    source: :course
+end
